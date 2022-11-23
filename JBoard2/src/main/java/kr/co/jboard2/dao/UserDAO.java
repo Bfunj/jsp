edbce57a2724655de2002da526f74a512df6d260
Bfunj <baekhjo9@gmail.com>
2022-11-23 13:52:51 +0900

    2022.11.23

--- JBoard2/src/main/java/kr/co/jboard2/dao/UserDAO.java
+++ JBoard2/src/main/java/kr/co/jboard2/dao/UserDAO.java
@@ -148,40 +148,81 @@ public class UserDAO extends DBHelper {
 		return vo;
 	}
 	
-public UserVO findUser(String name, String email) {
+public UserVO selectUserForFindId(String name, String email) {
 		
-		UserVO vo= null;
+		UserVO vo = null;
+		
 		try {
-			logger.info("findUser");
+			logger.info("selectUserForFindId...");
 			
-			conn= getConnection();
-			psmt = conn.prepareStatement(Sql.FIND_USER);
+			conn = getConnection();
+			psmt = conn.prepareStatement(Sql.SELECT_USER_FOR_FIND_ID);
 			psmt.setString(1, name);
 			psmt.setString(2, email);
-			rs=psmt.executeQuery();
+			rs = psmt.executeQuery();
 			
 			if(rs.next()) {
 				vo = new UserVO();
 				vo.setUid(rs.getString(1));
-				vo.setPass(rs.getString(2));
-				vo.setName(rs.getString(3));
-				vo.setNick(rs.getString(4));
-				vo.setEmail(rs.getString(5));
-				vo.setHp(rs.getString(6));
-				vo.setGrade(rs.getInt(7));
-				vo.setZip(rs.getString(8));
-				vo.setAddr1(rs.getString(9));
-				vo.setAddr2(rs.getString(10));
-				vo.setRegip(rs.getString(11));
-				vo.setRdate(rs.getString(12));
+				vo.setName(rs.getString(2));
+				vo.setEmail(rs.getString(3));
+				vo.setRdate(rs.getString(4));
 			}
 			close();
-		}catch(Exception e) {
+			
+		}catch (Exception e) {
 			logger.error(e.getMessage());
 		}
+		
 		return vo;
 	}
+
+
+public int selectUserForFindPw(String uid, String email) {
 	
+	int result = 0;
+	
+	try {
+		logger.info("selectUserForFindPw...");
+		
+		conn = getConnection();
+		psmt = conn.prepareStatement(Sql.SELECT_USER_FOR_FIND_PW);
+		psmt.setString(1, uid);
+		psmt.setString(2, email);
+		rs = psmt.executeQuery();
+		
+		if(rs.next()) {
+			result = rs.getInt(1);
+		}
+		close();
+		
+	}catch (Exception e) {
+		logger.error(e.getMessage());
+	}
+	
+	return result;
+}
+
+public int updateUserPassword(String uid, String pass) {
+	
+	int result = 0;
+	
+	try {
+		logger.info("updateUserPassword...");
+		
+		conn = getConnection();
+		psmt = conn.prepareStatement(Sql.UPDATE_USER_PASSWORD);
+		psmt.setString(1, pass);
+		psmt.setString(2, uid);
+		result = psmt.executeUpdate();
+		
+		close();			
+		
+	}catch (Exception e) {
+		logger.error(e.getMessage());
+	}
+	return result;
+}
 	public void selectUsers() {}
 	public void updateUser() {}
 	public void deleteUser() {}
--- JBoard2/src/main/webapp/index.jsp
+++ JBoard2/src/main/webapp/index.jsp
@@ -1,14 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Insert title here</title>
-	</head>
-	<body>
-	
-	
-	
-	</body>
-</html>
+<c:choose>
+	<c:when test="${empty sessUser }">
+		<jsp:forward page="/user/login.do"/>
+	</c:when>
+	<c:otherwise>
+	<jsp:forward page="/user/list.do"/>
+	</c:otherwise>
+</c:choose>
